54ac1e3ea018a7f44bd09668284f0d6fa40e552c
OmarRamzy45 <ramzyomar0@gmail.com>
2024-05-25 15:33:54 +0300

    add localization pkg

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -9,7 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/nvidia/roar_ws/src/JetsonTX2_WS/build/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/nvidia/roar_ws/src/JetsonTX2_WS/build/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/noetic/bin:/home/nvidia/.local/bin:/usr/local/cuda-10.2/bin:/home/nvidia/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PATH="/opt/ros/noetic/bin:$PATH"
 export PKG_CONFIG_PATH="/home/nvidia/roar_ws/src/JetsonTX2_WS/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/nvidia/roar_ws/src/JetsonTX2_WS/build'
 export PYTHONPATH="/home/nvidia/roar_ws/src/JetsonTX2_WS/build/devel/lib/python3/dist-packages:$PYTHONPATH"
